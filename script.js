1c346f47f34f9816b02508b7b757b23f951358c2
rishabh98080 <rishabh24273239pandey@gmail.com>
2025-12-14 00:02:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
+
 const addBtn = document.getElementById("addBtn");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
+if(localStorage.getItem('lastTaskCount') == undefined){
+    const lastTask = localStorage.setItem("lastTaskCount","task0");
+
+}
+function taskcreator(){
+    let number  = Number(localStorage.getItem('lastTaskCount').slice(4));
+    let newTaskName = "task" + Number(number + 1) ;
+    localStorage.setItem('lastTaskCount',newTaskName);
+    return String(newTaskName);
+}
+
+
+function writeData(newTaskName){
+    const localobjects = fetchData();
+    const kvalue = taskcreator();
+    localobjects[kvalue] = newTaskName;
+    for(let k in localobjects){
+        localStorage.setItem(k,localobjects[k]);
+    }
+    return [localobjects,kvalue];
+}
+function fetchData(){
+    const localobjects = () =>{
+        const newObject = {};
+        for(let i = 0 ; i < localStorage.length;i++){
+            const k = localStorage.key(i);
+            if(k.substring(0,4) == "task"){
+                newObject.k = localStorage.getItem(k);
+            }
+        }
+        return newObject;
+    }
+    return localobjects();
+}
 function addTask() {
     const taskName = taskInput.value.trim();
     if (taskName === "") return;
+
+    
 
     const li = document.createElement("li");
     li.textContent = taskName;
@@ -12,7 +49,10 @@ function addTask() {
     li.addEventListener("click", () => {
         li.classList.toggle("completed");
     });
+    const objectData = writeData(taskName);
+    li.id = objectData[1];
 
+    console.log(localStorage);
     const del = document.createElement("span");
     del.textContent = "Delete";
     del.className = "deleteBtn";
@@ -20,6 +60,8 @@ function addTask() {
     del.addEventListener("click", (e) => {
         e.stopPropagation();
         li.remove();
+        localStorage.removeItem(li.id);
+        console.log(localStorage);
     });
 
     li.appendChild(del);
@@ -31,3 +73,45 @@ addBtn.addEventListener("click", addTask);
 taskInput.addEventListener("keypress", (e) => {
     if (e.key === "Enter") addTask();
 });
+
+function listPopulator(){
+    
+    for(let i = 0 ; i < localStorage.length; i++){
+        const key = localStorage.key(i);
+
+        if(key.slice(0,4) == "task"){
+            const taskName = localStorage.getItem(key);
+            if (taskName === "") return;
+
+            const li = document.createElement("li");
+            li.textContent = taskName;
+
+            li.addEventListener("click", () => {
+                li.classList.toggle("completed");
+            });
+            li.id = key;
+            const del = document.createElement("span");
+            del.textContent = "Delete";
+            del.className = "deleteBtn";
+
+            del.addEventListener("click", (e) => {
+                e.stopPropagation();
+                li.remove();
+                localStorage.removeItem(key);
+                console.log(localStorage);
+            });
+            li.appendChild(del);
+            taskList.appendChild(li);
+            taskInput.value = "";
+        }
+
+        
+    }
+}
+function deleteTask(taskObject,taskNumber){
+    delete taskObject.taskNumber;
+    return taskObject;
+}
+{
+    listPopulator();
+}
