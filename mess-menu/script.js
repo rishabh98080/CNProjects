1fdfd2df341338b40939d8a395b94264419e7557
Rishabh Kumar <rishabh24273239pandey@gmail.com>
2025-12-28 00:28:29 +0530

    Added my-new-folder

---
+++ mess-menu/script.js
@@ -0,0 +1,66 @@
+const day = document.getElementById('day');
+const date = document.getElementById('date');
+const bfast = document.getElementById('bfast');
+const lunch = document.getElementById('lunch');
+const snack = document.getElementById('snack');
+const dinner = document.getElementById('dinner');
+const table = document.getElementById('table-menu');
+let sundays = 1;
+
+
+{
+const day1 = document.getElementById('day1');
+const date1 = document.getElementById('date1');
+const bfast1 = document.getElementById('bfast1');
+const lunch1 = document.getElementById('lunch1');
+const snack1 = document.getElementById('snack1');
+const dinner1 = document.getElementById('dinner1');
+}
+async function readData(){
+    try{
+        const response = await fetch("./mess_menu.json");
+        const data = await response.json();
+        console.log(data);
+        showData(data);
+        console.log(sundays);
+    }
+    catch(err){
+        console.log("Caught some error : \n" + err);
+    }
+}
+function showData(data){
+    const now = new Date();
+    const tday = now.toDateString().slice(0,4);
+    const tdate = now.toDateString().slice(4);
+    console.log(tday+ " " + tdate);
+
+    date.textContent = tdate;
+    for(let key in data){
+        if(tday.trim().toLowerCase() == "sun".trim()){
+            
+            if(key.trim().toLowerCase() == "sunday_even".trim()){
+                day.textContent = "Sunday";
+                bfast.textContent = data[key.trim()]["Breakfast".trim()];
+                lunch.textContent = data[key.trim()]["Lunch".trim()];
+                snack.textContent = data[key.trim()]["Snack".trim()];
+                dinner.textContent = data[key.trim()]["Dinner".trim()];
+            }
+            if(key.trim().toLowerCase() == "sunday_odd".trim()){
+                day1.textContent = "Sunday";
+                date1.textContent = tdate;
+                bfast1.textContent = data[key.trim()]["Breakfast".trim()];
+                lunch1.textContent = data[key.trim()]["Lunch".trim()];
+                snack1.textContent = data[key.trim()]["Snack".trim()];
+                dinner1.textContent = data[key.trim()]["Dinner".trim()];
+            }
+        }
+        else{
+            day.textContent = tday;
+            bfast.textContent = data[key.trim()]["Breakfast".trim()];
+            lunch.textContent = data[key.trim()]["Lunch".trim()];
+            snack.textContent = data[key.trim()]["Snack".trim()];
+            dinner.textContent = data[key.trim()]["Dinner".trim()];
+        }
+    }
+}
+readData();
